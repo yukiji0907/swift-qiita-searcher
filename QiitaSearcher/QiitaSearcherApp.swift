e1988a2ad74180ba27706dc9778d0b5dcd7ec94b
yukiji0907 <y.genda.dev@gmail.com>
2023-02-25 13:48:19 +0900

    Initial Commit

---
+++ QiitaSearcher/QiitaSearcherApp.swift
@@ -0,0 +1,18 @@
+//
+//  QiitaSearcherApp.swift
+//  QiitaSearcher
+//  
+//  Created by yukiji0907 on 2023/02/25
+//  
+
+
+import SwiftUI
+
+@main
+struct QiitaSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
